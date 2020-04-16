90261b1fe01e5c0b2f514e78d4fbb0971f8ef57f
Venkatesh Rajendran <ven2701@gmail.com>
2020-04-16 18:05:47 +0530

    Added Components and Basic Impl

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
+import Layout from './components/layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component{
   render(){
     return (
       <div className={classes.App}>
-        Hello World
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     )
   }
