8e69089c2d5c017a84f2f37fad8d26dc63026de5
Venkatesh Rajendran <ven2701@gmail.com>
2020-04-16 18:55:05 +0530

    Added Ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css'
 
 const Burger = props => {
+    let burgerIngredients = Object.keys(props.ingredients).map(ingredient => {
+        return [...Array(props.ingredients[ingredient])].map((_,i) => {
+            return <BurgerIngredient type={ingredient} key={ingredient+i}/>
+        })
+    }).reduce((ar,el)=>{
+        return ar.concat(el);
+    },[]);
+    if(burgerIngredients.length <= 0){
+        burgerIngredients = <p>Please add some ingredients</p>
+    }
+    console.log(burgerIngredients)
     return(
+        
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {burgerIngredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Auxilary from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
+
+    state = {
+        ingredient : {
+            salad : 0,
+            bacon : 0,
+            cheese : 2,
+            meat : 1
+        }
+    }
+
     render(){
         return(
             <Auxilary>
-                <Burger />
+                <Burger ingredients={this.state.ingredient} />
                 <div>Build Controls</div>
             </Auxilary>
         );
