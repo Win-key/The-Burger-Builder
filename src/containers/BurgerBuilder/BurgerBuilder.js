50a1973511ada991e69a97d3e58b6ffabc480ba3
Venkatesh Rajendran <ven2701@gmail.com>
2020-04-21 17:00:26 +0530

    Binded with firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
+
 import Auxilary from '../../hoc/Auxilary';
 import Burger from '../../components/Burger/Burger';
 import Modal from '../../UI/Modal/Modal';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../UI/Spinner/Spinner';
 
 const INGREDIENT_PRICE = {
     salad : 0.4,
@@ -23,7 +26,8 @@ class BurgerBuilder extends Component{
         },
         totalPrice : 0,
         purchasable : false,
-        purchasing : false
+        purchasing : false,
+        loading : false
     }
 
     addIngredientHandler = type=>{
@@ -66,23 +70,54 @@ class BurgerBuilder extends Component{
             return {purchasing : false}
         });
     }
+
+    purchaseContinueHandler = () => {
+        console.log('buy');
+        this.setState({loading : true});
+        const order = {
+            ingredients : this.state.ingredient,
+            price : this.state.totalPrice,
+            customer : {
+                name : 'Winkey',
+                address : {
+                    street : 'Test Street',
+                    pincode : '010101',
+                    country : 'India'
+                },
+                email : 'winkey@gmail.com'
+            },
+            deliveryMethod : 'fastest',
+            deliveryComment : 'Vegama vada .. Paradesi paradesi.. Paduthuranya'
+        }
+        axios.post('orders.json',order)
+            .then(response =>{
+                this.setState({loading : false, purchasing : false});
+            })
+            .catch(error =>{
+                this.setState({loading : false, purchasing : false});
+            });
+    }
     
     render(){
         let disabledInfo = {...this.state.ingredient};
         for(let ingr in disabledInfo){
             disabledInfo[ingr] = disabledInfo[ingr] <=0;
         }
+        let orderSummary = <Spinner />;
+        if(!this.state.loading){
+            orderSummary = <OrderSummary 
+                            ingredients={this.state.ingredient} 
+                            purchasable={this.state.purchasable}
+                            totalPrice= {this.state.totalPrice.toFixed(2)}
+                            modalClose={this.modalCloseHandler}
+                            buy = {this.purchaseContinueHandler}/>
+        }
         return(
             <Auxilary>
                 <Modal 
                     show={this.state.purchasing}
                     modalClose={this.modalCloseHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredient} 
-                        purchasable={this.state.purchasable}
-                        totalPrice= {this.state.totalPrice.toFixed(2)}
-                        modalClose={this.modalCloseHandler}
-                        buy = {()=>{console.log('Buy')}}/>
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredient} />
                 <BuildControls 
